d922d761d677e08833492a72458304cc9176264e
maintainline <z.seon.dev@gmail.com>
2025-06-18 16:08:26 +0900

    [docs] TypeScript 기본자료 형

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-import { hello } from "./hello";
-hello();
+let anyVar = 100;
+anyVar = "안녕";
+anyVar = true;
+anyVar = null;
+anyVar = undefined;
+anyVar = symbol("age");
+anyVar = BigInt(999999999);
+export {};
